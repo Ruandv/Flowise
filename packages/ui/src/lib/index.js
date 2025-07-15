b4b72bfcba709de7af851af7c11f11051ecd77ea
Ruan De Villiers <Ruan.DeVilliers@nintex.com>
2025-07-15 08:46:50 +0200

    chore: wip

--- packages/ui/src/lib/index.js
+++ packages/ui/src/lib/index.js
@@ -3,3 +3,4 @@
 export { FlowiseProvider } from './FlowiseProvider'
 export { default as AgentFlows } from '../views/agentflows'
 export { default as FlowiseCanvas } from '../views/agentflowsv2/Canvas'
+export { default as FlowiseAPIKey } from '../views/apikey'
